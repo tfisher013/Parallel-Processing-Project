d30df820d5c0bb90f5bb4c7427d48ffe672c79f9
Thomas Fisher <tfisher013@gmail.com>
2023-11-15 21:39:03 -0700

    Added check for file name arg

--- util/parse matrix file/test_matrix_parser.c
+++ util/parse matrix file/test_matrix_parser.c
@@ -4,13 +4,20 @@
 /**
  * Demonstration of how to use matrix file parser,
  * in 3 easy steps! :))))
- * 
+ *
  * To compile and run this example:
  * gcc test_matrix_parser.c read_file_matrix.c -o test_matrix_parser
  * ./ test_matrix_parser cage3.mtx
  */
 int main(int argc, char *argv[])
 {
+
+    if (argc == 1)
+    {
+        printf("Please provide a file name parameter. Exiting.\n");
+        return 0;
+    }
+
     // 1. get the dimension of the matrix from the file
     // (assumed to be square) by passing file name to
     // the method below
