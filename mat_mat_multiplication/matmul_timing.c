7e7c02f1c209f829c888fed5e0eaccbe0961ba46
PraneethMarri <praneeth102@unm.edu>
2023-11-26 16:59:16 -0700

    created mat_mat_mul timings file

---
+++ mat_mat_multiplication/matmul_timing.c
@@ -0,0 +1,7 @@
+#include <mpi.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "../util/parse_matrix_file/read_matrix_file.h"
+#include "../util/format_datatypes/COO.h"
+#include "../util/format_datatypes/CSR.h"
+#include "../util/format_datatypes/CSC.h"
