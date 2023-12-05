8dd92138e0cc89acafc56a7f540cc46c6b60eaa4
Thomas Fisher <80178894+tfisher013@users.noreply.github.com>
2023-12-05 16:20:29 -0700

    COO and CSR serial matmat working

--- mat_mat_multiplication/matmat.c
+++ mat_mat_multiplication/matmat.c
@@ -3,8 +3,48 @@
 #include <time.h>
 #include "../../util/parse_matrix_file/read_matrix_file.h"
 #include "../../util/format_datatypes/COO.h"
+#include "../../util/format_datatypes/CSR.h"
 
-void matmat(COO *coo_A, COO *coo_B, int dim){
+void matmatCSR(CSR *csr_A, CSR *csr_B, int dim){
+
+    int non_zero_val_counter = 0;
+        for(int row = 0; row < dim; row++){
+            for(int col = 0; col < dim; col++){
+                double tmp = 0;
+                for(int j=0; j < dim ; j++){
+
+                    int A_val_index = -1; 
+                    int B_val_index = -1;
+
+                    // satifies finding nnz in A at row, col
+                    for(int k = csr_A->rowPtrs[row]; k < csr_A->rowPtrs[row+1]; k++){
+                        if(csr_A->cols[k] == j){
+                            A_val_index = k;
+                            break;
+                        }
+                    }
+
+                    // do the same for B
+                    for(int k = 0; k < csr_A->nnz; k++){
+                        if(csr_A->cols[k] == col){
+                            if(k >= csr_A->rowPtrs[j] && k < csr_A->rowPtrs[j+1]){
+                                B_val_index = k;
+                                break;
+                            }
+                        }
+                    }
+
+                    if(A_val_index >= 0 && B_val_index >= 0){
+                        tmp += csr_A->values[A_val_index] * csr_B->values[B_val_index];
+                    }
+                }
+                printf("The value at row %d and col %d is %f\n", row, col, tmp);
+                non_zero_val_counter++;
+            }
+        }
+}
+
+void matmatCOO(COO *coo_A, COO *coo_B, int dim){
 
     int non_zero_val_counter = 0;
         for(int row = 0; row < dim; row++){
@@ -39,7 +79,7 @@ void matmat(COO *coo_A, COO *coo_B, int dim){
 
 int main(int argc, char *argv[]){
 
-    char *filename = "../../matrices/standardized_matrices/dimension_100_nonzeros_100.mtx";
+    char *filename = "identity2.mtx";
     int dim = getStandardMatrixDimension(filename);
     double *matrix_A = calloc(dim * dim, sizeof(double));
     //double *matrix_B = calloc(dim * dim, sizeof(double));
@@ -48,13 +88,15 @@ int main(int argc, char *argv[]){
 
     // perform COO matmul timing
     COO coo_A;
+    CSR csr_A;
    // COO coo_B;
 
     convertToCOO(&coo_A, dim, dim, matrix_A);
+    convertToCSR(&csr_A, dim, dim, matrix_A);
     //printCOO(&coo_A);
     //convertToCOO(&coo_B, dim, dim, matrix_B);
-    matmat(&coo_A, &coo_A, dim);
-    freeCOO(&coo_A);
+    matmatCSR(&csr_A, &csr_A, dim);
+    freeCSR(&csr_A);
     //freeCOO(&coo_B);
 
 
