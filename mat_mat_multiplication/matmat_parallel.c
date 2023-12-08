200084bc14615f52488384f0184e7df92324807f
Thomas Fisher <tfisher013@gmail.com>
2023-12-08 14:57:37 -0700

    Parallel matmat working for 2 procs

--- mat_mat_multiplication/matmat_parallel.c
+++ mat_mat_multiplication/matmat_parallel.c
@@ -5,6 +5,7 @@
 #include "../util/parse_matrix_file/read_matrix_file.h"
 #include "../util/format_datatypes/COO.h"
 
+
 double *transposeMat(double *mat, int dim)
 {
     double *transpose = calloc(dim * dim, sizeof(double));
@@ -20,9 +21,25 @@ double *transposeMat(double *mat, int dim)
 
 void cooMat(COO *coo_A, COO *coo_B, COO *coo_C, int dim, int rank)
 {
+
+    int offset = 0;
+    if(rank == 1){
+        offset += dim / 2;
+    }
+
+    int rowPtrStart = 0;
+    if(rank == 1){
+        for(int i = 0; i < coo_A->nnz; i++){
+            if(coo_A->rows[i] >= dim / 2){
+                rowPtrStart = i;
+                break;
+            }
+        }
+    }
+
     int counter = 0;
-    int constRowPtrA = 0;
-    for (int i = 0; i < dim; i++)
+    int constRowPtrA = rowPtrStart;
+    for (int i = offset; i < offset + dim / 2; i++)
     {
         int rowPtrB = 0;
         for (int j = 0; j < dim; j++)
@@ -49,7 +66,7 @@ void cooMat(COO *coo_A, COO *coo_B, COO *coo_C, int dim, int rank)
                 }
             }
             // TODO What do we do w/ coo_C ?
-            printf("Process %d: value at row %d col %d is %f\n", i, j, tmp, rank);
+            printf("Process %d: value at row %d col %d is %f\n", rank, i, j, tmp);
         }
     }
 
@@ -77,69 +94,31 @@ int main(int argc, char *argv[])
     }
     MPI_Bcast(&dim, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
-    // each process allocates their own partition, root will fill
-    // them with correct values
-    double *matrix_A = calloc(dim * dim / procs, sizeof(double));
-    double *matrix_B = calloc(dim * dim / procs, sizeof(double));
+    int partition_dim = dim / sqrt(procs);
 
-    if (rank == 0)
-    {
-        double *full_matrix_A = calloc(dim * dim, sizeof(double));
-        double *full_matrix_B = calloc(dim * dim, sizeof(double));
+    double *full_matrix_A = calloc(dim * dim, sizeof(double));
+    double *full_matrix_B = calloc(dim * dim, sizeof(double));
 
-        getStandardMatrix(argv[1], dim, full_matrix_A);
-        getStandardMatrix(argv[2], dim, full_matrix_B);
+    getStandardMatrix(argv[1], dim, full_matrix_A);
+    getStandardMatrix(argv[2], dim, full_matrix_B);
 
-        full_matrix_B = transposeMat(full_matrix_B, dim);
+    double *full_matrix_B_tranpose = transposeMat(full_matrix_B, dim);
 
-        for (int i = 1; i < procs; i++)
-        {
-            int row_offset = (i % dim);
-            int col_offset = (i / dim);
-            for (int j = 0; j < dim / sqrt(procs); j++)
-            {
-                for (int k = 0; k < dim / sqrt(procs); k++)
-                {
-                    matrix_A[j * dim + k] = full_matrix_A[(row_offset + j) * dim + (k + col_offset)];
-                    matrix_B[j * dim + k] = full_matrix_B[(row_offset + j) * dim + (k + col_offset)];
-                }
-            }
+    COO full_coo_A, full_coo_B;//, coo_C;
+    convertToCOO(&full_coo_A, dim, dim, full_matrix_A);
+    convertToCOO(&full_coo_B, dim, dim, full_matrix_B_tranpose);
 
-            MPI_Send(matrix_A, dim * dim / procs, MPI_DOUBLE, i, 1234, MPI_COMM_WORLD);
-            MPI_Send(matrix_B, dim * dim / procs, MPI_DOUBLE, i, 5678, MPI_COMM_WORLD);
-        }
-
-        // root process finally creates it own partition
-        for (int j = 0; j < dim / sqrt(procs); j++)
-        {
-            for (int k = 0; k < dim / sqrt(procs); k++)
-            {
-                matrix_A[j * dim + k] = full_matrix_A[j * dim + k];
-                matrix_B[j * dim + k] = full_matrix_B[j * dim + k];
-            }
-        }
-
-        free(full_matrix_A);
-        free(full_matrix_B);
-    }
-    else
-    {
-        MPI_Recv(matrix_A, dim * dim / procs, MPI_DOUBLE, 0, 1234, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        MPI_Recv(matrix_B, dim * dim / procs, MPI_DOUBLE, 0, 5678, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-    }
-
-    COO coo_A, coo_B, coo_C;
-    convertToCOO(&coo_A, dim, dim, matrix_A);
-    convertToCOO(&coo_B, dim, dim, matrix_B);
-
-    cooMat(&coo_A, &coo_B, NULL, dim, 0);
+    MPI_Barrier(MPI_COMM_WORLD);
+    cooMat(&full_coo_A, &full_coo_B, NULL, dim, rank);
     MPI_Barrier(MPI_COMM_WORLD);
 
-    freeCOO(&coo_A);
-    freeCOO(&coo_B);
+    freeCOO(&full_coo_A);
+    freeCOO(&full_coo_B);
 
-    free(matrix_A);
-    free(matrix_B);
+    free(full_matrix_A);
+    free(full_matrix_B);
+
+    free(full_matrix_B_tranpose);
 
     MPI_Finalize();
     return 0;
